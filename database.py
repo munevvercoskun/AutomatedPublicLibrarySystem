690f3ed91c6a688f65c1bb87c697ae0104996c17
munevvercoskun <mnvvr1907@gmail.com>
2024-04-01 19:08:21 -0400

    Updated the file

--- database.py
+++ database.py
@@ -1,13 +1,13 @@
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-db_connection_string = "mysql+pymysql://sqlserver:sqlserver@p23281621142-u4q1my@gcp-sa-cloud-sql.iam.gserviceaccount.com/AutomatedPublicLibrarySystem?charset=utf8mb4"
+
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://coe892:PublicLibrarySystem...@coe892:1433/AutomatedPublicLibrary"
 
 engine = create_engine(
-  db_connection_string, 
-  connect_args={
-    "ssl": {
-      "ssl_ca": "/etc/ssl/cert.pem"
-    }
-  })
-with engine.connect() as conn:
-    query = text("SELECT * FROM user")
+    SQLALCHEMY_DATABASE_URL, connect_args={}, echo=True
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
